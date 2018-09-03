6886470db5e2ff3b01b692315c6fad734434e20a
Anthony Febus <febus.anthony@gmail.com>
2018-09-03 02:21:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function theBeatlesPlay(musicians, instruments) {
   let result = [ ];
     for (i = 0; i < 3; i++) {
-       //console.log (musicians[i])
+       console.log (musicians[i])
+        result.push (m[i] + " plays " + i[i])
           }
   result.push ()
 } 
